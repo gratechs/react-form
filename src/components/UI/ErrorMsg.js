d5b19fb372838c770d6d7f9c3246bb034560c3fb
gratechs <godimfon@gmail.com>
2022-09-19 07:33:06 +0100

    Designed error message

---
+++ src/components/UI/ErrorMsg.js
@@ -0,0 +1,12 @@
+const ErrorMsg = () => {
+  return (
+    <div className="flex items-center justify-center space-x-2 container mx-auto mb-4 p-1 text-white text-sm bg-red-600 border-2 border-red-400 rounded-lg sm:w-9/12 md:w-10/12 lg:w-9/12 lg:space-x-8 xl:space-x-12">
+      <p>Incorrect details</p>
+      <button className="font-semibold text-2xl border-none hover:text-red-400">
+        &times;
+      </button>
+    </div>
+  );
+};
+
+export default ErrorMsg;
